8bd536d7a922856200412116d2ce62aac90852b4
mikewanjero <mikewanj@gmail.com>
2025-01-30 16:51:54 +0300

    react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import PersonalInfo from "./components/PersonalInfo";
 import Education from "./components/Education";
 import WorkExperience from "./components/WorkExperience";
@@ -11,7 +16,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<PersonalInfo />} />
+        <Route path="/" element={<Navigate to="/step1" />} />
+        <Route path="/step1" element={<PersonalInfo />} />
         <Route path="/education" element={<Education />} />
         <Route path="/work-experience" element={<WorkExperience />} />
         <Route path="/user-photo" element={<UserPhoto />} />
