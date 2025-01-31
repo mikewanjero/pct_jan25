d3bce64a9d8bfcf24e4e6f0d28cca6d7644f891c
mikewanjero <mikewanj@gmail.com>
2025-01-31 10:37:58 +0300

    Update app routing - company template

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import PersonalInfo from "./components/PersonalInfo";
-import Education from "./components/Education";
-import WorkExperience from "./components/WorkExperience";
-import UserPhoto from "./components/UserPhoto";
-import "../src/styles/custom.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TrainingSheet from "./components/TrainingSheet";
+import MasterDoc from "./components/MasterDoc";
+import ActActivation from "./components/ActActivation";
+import "./styles/custom.scss";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/step1" />} />
-        <Route path="/step1" element={<PersonalInfo />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/work-experience" element={<WorkExperience />} />
-        <Route path="/user-photo" element={<UserPhoto />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<TrainingSheet />} />
+          <Route path="/step2" element={<MasterDoc />} />
+          <Route path="/step3" element={<ActActivation />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
