759cb9679bdcd30dd05bd29dbf69e8ae20059990
mikewanjero <mikewanj@gmail.com>
2025-01-31 13:52:41 +0300

    sidebar with step counter

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,25 @@
 /* eslint-disable react/prop-types */
-import { ListGroup } from "react-bootstrap";
-import { FaCheckCircle } from "react-icons/fa";
-
 const Sidebar = ({ currentStep }) => {
+  const steps = [
+    "Upload Training Document",
+    "Upload Master Document",
+    "Activate Account",
+  ];
+
   return (
-    <ListGroup>
-      <ListGroup.Item active={currentStep === 1}>
-        {currentStep > 1 && <FaCheckCircle className="text-success" />} Step 1:
-        Upload Training Sheet
-      </ListGroup.Item>
-      <ListGroup.Item active={currentStep === 2}>
-        {currentStep > 2 && <FaCheckCircle className="text-success" />} Step 2:
-        Upload Master Docs
-      </ListGroup.Item>
-      <ListGroup.Item active={currentStep === 3}>
-        {currentStep === 3 && <FaCheckCircle className="text-success" />} Step
-        3: Activate Account
-      </ListGroup.Item>
-    </ListGroup>
+    <div className="sidebar">
+      <h3>Step {currentStep + 1}</h3>
+      <div className="sidebar-steps">
+        {steps.map((step, index) => (
+          <p
+            key={index}
+            className={`step ${index === currentStep ? "active" : ""}`}
+          >
+            {step}
+          </p>
+        ))}
+      </div>
+    </div>
   );
 };
 
