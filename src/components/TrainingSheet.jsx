4c55d1f228f58108edb5b71cc3bd331663cda9dc
mikewanjero <mikewanj@gmail.com>
2025-02-03 14:01:46 +0300

    header

--- src/components/TrainingSheet.jsx
+++ src/components/TrainingSheet.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
+import phamacoreLogo from "../assets/images/phamacore.png";
 import Sidebar from "./Sidebar";
 
 const TrainingSheet = () => {
@@ -11,6 +12,13 @@ const TrainingSheet = () => {
         <Sidebar currentStep={0} />
 
         <div className="form-content">
+          {/* Header */}
+          <div className="d-flex flex-column align-items-center mb-4">
+            <img src={phamacoreLogo} alt="logo" className="w-40 h-40" />
+            <h1 className="text-caramel display-5 fw-bold">
+              phAMACore<sup>™</sup>Cloud
+            </h1>
+          </div>
           <div className="form-header">
             <h2>Upload Required Training Document</h2>
             <p>Max file size: 5MB | Max 3 files</p>
