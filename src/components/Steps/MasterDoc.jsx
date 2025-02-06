1eb250a6b94bf18f262104f668a4cf65b0555de2
mikewanjero <mikewanj@gmail.com>
2025-02-06 14:45:04 +0300

    Upload form layout change

--- src/components/Steps/MasterDoc.jsx
+++ src/components/Steps/MasterDoc.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
+import FileUpload from "../Upload/FileUpload";
 import phamacoreLogo from "../../assets/images/phamacore.png";
 import Sidebar from "../Sidebar/Sidebar";
 
@@ -21,16 +22,15 @@ const MasterDoc = () => {
           </div>
           <div className="form-header">
             <h2>Upload Master Document (Optional)</h2>
-            <p>Max file size: 5MB | Max 3 files</p>
           </div>
 
           <Form>
-            <Form.Group controlId="masterFile">
-              <div className="upload-box">
-                <p>Click or Drag files here to upload</p>
-                <Form.Control type="file" multiple hidden />
-              </div>
-            </Form.Group>
+            <div className="upload-box">
+              <p>Max file size: 5MB | Max 3 files</p>
+              <FileUpload
+                onFileUpload={(files) => console.log("Uploaded files:", files)}
+              />
+            </div>
 
             <div className="d-flex justify-content-between mt-3">
               <Button
