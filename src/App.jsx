1aef14e2d8031f79f91ff7c65ea2f0a423e58161
mikewanjero <mikewanj@gmail.com>
2025-02-26 16:27:08 +0300

    Removed upload files as steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TrainingSheet from "./components/Steps/TrainingSheet";
-import MasterDoc from "./components/Steps/MasterDoc";
 import ActActivation from "./components/Steps/ActActivation";
 import "./styles/custom.scss";
 
@@ -9,8 +7,6 @@ function App() {
     <Router>
       <div className="container">
         <Routes>
-          <Route path="/" element={<TrainingSheet />} />
-          <Route path="/master-doc-upload" element={<MasterDoc />} />
           <Route path="/acct-activation" element={<ActActivation />} />
         </Routes>
       </div>
