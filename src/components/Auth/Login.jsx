dc98fd609009676670e7ab1703da4b4e23df194e
mikewanjero <mikewanj@gmail.com>
2025-02-28 13:07:42 +0300

    Creating login form

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="container-fluid min-vh-100 d-flex align-items-center">
+      <div className="mx-auto">
+        <div className="card login-card shadow">
+          <div className="card-body p-4">
+            <div>Login</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
