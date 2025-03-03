274a45b0dac66e7e9b9a23df0833865800f32da3
mikewanjero <mikewanj@gmail.com>
2025-03-03 16:34:56 +0300

    new routes 'forgot password' & 'd.p.a'

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ActActivation from "./components/Steps/ActActivation";
 import Login from "./components/Auth/Login";
 import "./styles/custom.scss";
+import ForgotPassword from "./components/Auth/ForgotPassword";
+import Dpa from "./components/util/dpa";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route index path="/" element={<Login />} />
           <Route path="/acct-activation" element={<ActActivation />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/privacy-policy" element={<Dpa />} />
         </Routes>
       </div>
     </Router>
