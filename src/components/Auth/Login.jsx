b7ac41d265ea267e3bf741b52d4a37c0af3decf7
mikewanjero <mikewanj@gmail.com>
2025-03-05 13:07:14 +0300

    login function, post api for user login

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import corebaseLogo from "../../assets/images/corebaseLogo.png";
 import phamacoreLogo from "../../assets/images/phamacoreLogo.png";
 import {
@@ -13,9 +14,49 @@ import {
 } from "react-bootstrap";
 import { FormGroup } from "react-bootstrap";
 import { BsLockFill } from "react-icons/bs";
+import axios from "axios";
 
 export default function Login() {
   const [agreed, setAgreed] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  // Function to handle user login
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!formData.password && !formData.username) {
+      setError("Username and Password are required!");
+      return;
+    }
+
+    setLoading(true);
+    setError("");
+
+    try {
+      const response = await axios.post("api_url", {
+        username: formData.username,
+        password: formData.password,
+      });
+
+      if (response.status === 200) {
+        const { token } = response.data;
+        localStorage.setItem("authToken", token);
+        navigate("/acct-activation");
+      } else {
+        setError("Invalid credentials. Please try again");
+      }
+    } catch (error) {
+      setError("Failed to login. Please check your credentials.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="container-fluid d-flex align-items-center">
@@ -38,14 +79,21 @@ export default function Login() {
                 </p>
               </div>
             </div>
-            <Form autoComplete="off">
+            {error && <p className="text-danger text-center">{error}</p>}
+            <Form autoComplete="off" onSubmit={handleLogin}>
               <FormGroup className="mb-3">
                 <FormLabel
                   style={{ fontSize: 13, color: "rgb(150, 150, 150)" }}
                 >
                   User Name
                 </FormLabel>
-                <FormControl type="username" />
+                <FormControl
+                  type="username"
+                  value="username"
+                  onChange={(e) =>
+                    setFormData({ ...formData, username: e.target.value })
+                  }
+                />
               </FormGroup>
               <FormGroup className="mb-3">
                 <FormLabel
@@ -53,7 +101,13 @@ export default function Login() {
                 >
                   Password
                 </FormLabel>
-                <FormControl type="password" />
+                <FormControl
+                  type="password"
+                  value={"password"}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                />
               </FormGroup>
 
               <FormGroup>
@@ -87,9 +141,10 @@ export default function Login() {
                 <Button
                   type="submit"
                   className="auth auth-btn btn-sm"
-                  disabled={!agreed}
+                  disabled={!agreed || loading}
+                  onSubmit={handleLogin}
                 >
-                  Login
+                  {loading ? "Logging in..." : "Login"}
                 </Button>
               </div>
               <div className="d-flex justify-content-center mt-2">
