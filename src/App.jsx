39dc6a258e586510d7bff1ec0b494b26d9d68633
mikewanjero <mikewanj@gmail.com>
2025-03-18 13:47:50 +0300

    new route - `reset password`

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import ActActivation from "./components/Steps/ActActivation";
 import Login from "./components/Auth/Login";
 import "./styles/custom.scss";
 import ForgotPassword from "./components/Auth/ForgotPassword";
+import ResetPassword from "./components/Auth/ResetPassword";
 import Dpa from "./components/util/dpa";
-
 function App() {
   return (
     <Router>
@@ -13,7 +13,8 @@ function App() {
           <Route index path="/" element={<Login />} />
           <Route path="/acct-activation" element={<ActActivation />} />
           <Route path="/acct-activation/:id" element={<ActActivation />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/privacy-policy" element={<Dpa />} />
         </Routes>
       </div>
