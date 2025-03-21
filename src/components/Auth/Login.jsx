d09d3b48216519b261e2a5531b5df323b6e4c93d
mikewanjero <mikewanj@gmail.com>
2025-03-21 11:55:22 +0300

    #

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -17,7 +17,7 @@ import { FormGroup, Toast, ToastContainer } from "react-bootstrap";
 import { BsLockFill, BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 import axios from "axios";
 
-const API_URL = "http://20.164.20.36:86/api/client";
+const API_URL = "http://20.164.20.36:86/";
 const API_HEADER = {
   accesskey: "R0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9",
   // "Content-Type": "multipart/form-data",
