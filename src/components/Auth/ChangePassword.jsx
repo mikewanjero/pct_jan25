d720c8213e24cec13872cd2ea3247b276e309a9e
mikewanjero <mikewanj@gmail.com>
2025-03-21 15:21:21 +0300

    new page -`Change Password`

---
+++ src/components/Auth/ChangePassword.jsx
@@ -0,0 +1,102 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Card,
+  CardBody,
+  Form,
+  FormGroup,
+  FormLabel,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import phamacoreLogo from "../../assets/images/phamacoreLogo.png";
+import corebaseLogo from "../../assets/images/corebaseLogo.png";
+
+export default function ChangePassword() {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="container-fluid d-flex align-items-center"
+      style={{ minWidth: 450, minHeight: 400 }}
+    >
+      <div className="mx-auto">
+        <Card className="shadow" style={{ width: "25rem" }}>
+          <CardBody className="p-4">
+            <div className="text-center">
+              <img
+                src={phamacoreLogo}
+                alt="brand"
+                className="d-flex img-fluid justify-content-center m-auto"
+                width={120}
+              />
+            </div>
+            <h2 className="text-center fs-4">Change Password</h2>
+            <p
+              className="text-center text-secondary p-1 mb-2"
+              style={{ fontSize: 12 }}
+            >
+              Please enter a new password
+            </p>
+            <Form autoComplete="off">
+              <div className="input-column">
+                <FormGroup className="mb-2">
+                  <FormLabel>New Password</FormLabel>
+                  <FormControl
+                    type="password"
+                    placeholder="Enter your new password"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel>Confirm New Password</FormLabel>
+                  <FormControl
+                    type="password"
+                    placeholder="Confirm your new password"
+                  />
+                </FormGroup>
+              </div>
+              <div className="d-flex justify-content-between gap-2 mt-3">
+                <Button
+                  className="btn-sm"
+                  onClick={() => navigate("/login")}
+                  style={{
+                    backgroundColor: "#28A745",
+                    borderColor: "rgb(79, 204, 48)",
+                    width: 150,
+                  }}
+                >
+                  Reset Password
+                </Button>
+                {/* <Button
+                      className="btn-sm"
+                      variant="secondary"
+                      onClick={() => navigate("/")}
+                      style={{
+                        backgroundColor: "rgb(197, 140, 79)",
+                        borderColor: "rgb(197, 140, 79)",
+                        width: 150,
+                      }}
+                    >
+                      Back to Login
+                    </Button> */}
+              </div>
+            </Form>
+          </CardBody>
+        </Card>
+        <footer id="footer">
+          <div className="copy-right text-center my-2">
+            <p className="m-0 company-sm">Powered by</p>
+            <img
+              src={corebaseLogo}
+              width={15}
+              className="img-fluid"
+              alt="company brand logo"
+            />
+            <p className="m-0 company-lg">CoreBase Solutions</p>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+}
