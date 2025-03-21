56cb51bdde63b946c861c1d33bec3e1a75093aa8
mikewanjero <mikewanj@gmail.com>
2025-03-21 15:23:44 +0300

    added new navigation route - `change password`

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ActActivation from "./components/Steps/ActActivation";
 import Login from "./components/Auth/Login";
 import "./styles/custom.scss";
+import ChangePassword from "./components/Auth/ChangePassword";
 import ForgotPassword from "./components/Auth/ForgotPassword";
 import ResetPassword from "./components/Auth/ResetPassword";
 import Dpa from "./components/util/dpa";
+
 function App() {
   return (
     <Router>
@@ -13,6 +15,7 @@ function App() {
           <Route index path="/" element={<Login />} />
           <Route path="/acct-activation" element={<ActActivation />} />
           <Route path="/acct-activation/:id" element={<ActActivation />} />
+          <Route path="/change-password" element={<ChangePassword />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/privacy-policy" element={<Dpa />} />
